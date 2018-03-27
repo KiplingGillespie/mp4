8217172e902b23916451e6903669cc720f70295b
Kip <kiplinggillespie@gmail.com>
2018-03-27 15:31:41 -0400

    added cs498TestClass.java

---
+++ jenkins/test/src/main/java/jenkins/model/cs498TestClass.java
@@ -0,0 +1,29 @@
+/* Test File */
+
+package jenkins.model;
+
+import hudson.DescriptorExtensionList;
+import hudson.Extension;
+import hudson.ExtensionPoint;
+import hudson.model.AbstractDescribableImpl;
+import hudson.util.CaseInsensitiveComparator;
+import org.apache.commons.lang.StringUtils;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import javax.annotation.Nonnull;
+import java.util.Comparator;
+import java.util.Locale;
+
+public class TestIdFromFilename{
+	
+	@Test
+	public void testIdEqual(){
+		
+	}
+	
+	@Test
+	public void testIDNull(){
+		
+	}
+}
+
