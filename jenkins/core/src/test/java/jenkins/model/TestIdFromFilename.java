d73e962b65c2facc9fe6cfd21ff338564a4da8bf
Kip <kiplinggillespie@gmail.com>
2018-04-04 16:43:13 -0400

    q4

--- jenkins/core/src/test/java/jenkins/model/TestIdFromFilename.java
+++ jenkins/core/src/test/java/jenkins/model/TestIdFromFilename.java
@@ -2,33 +2,41 @@
 
 package jenkins.model;
 
-import hudson.DescriptorExtensionList;
-import hudson.Extension;
-import hudson.ExtensionPoint;
-import hudson.model.AbstractDescribableImpl;
-import hudson.util.CaseInsensitiveComparator;
-import org.apache.commons.lang.StringUtils;
-import org.kohsuke.stapler.DataBoundConstructor;
+import hudson.Util;
+import hudson.util.StreamTaskListener;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Map;
+import java.util.TimeZone;
+import java.util.TreeMap;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import org.apache.commons.io.FileUtils;
 
-import javax.annotation.Nonnull;
-import java.util.Comparator;
-import java.util.Locale;
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 public class TestIdFromFilename{
-	String Filename = "Thisisafilename";
+	String Filename = "[a-z0-9_. -]+";
 	
-	
+	@Test
 	public void testIdEqual(){
-		AbstractDescribableImpl.IdStrategy bob = new AbstractDescribableImpl.IdStrategy();
-		String result = bob.idFromFilename(Filename);
-		assertEquals(result, Filename);
+		jenkins.model.IdStrategy.CaseSensitive cs;
+		cs = new jenkins.model.IdStrategy.CaseSensitive();
+		String result = cs.idFromFilename(Filename);
+		assertEquals(result, "a-z0-9_. -");
 	}
 	
-	
+	@Test
 	public void testIdNull(){
-		AbstractDescribableImpl.IdStrategy bob = new AbstractDescribableImpl.IdStrategy();
-		String result = bob.idFromFilename(null);
-		asserNull(result);
+		jenkins.model.IdStrategy.CaseSensitive cs;
+		cs = new jenkins.model.IdStrategy.CaseSensitive();
+		String result = cs.idFromFilename("$abcd");
+		assertEquals(result, "ꯍ");
+
 	}
 }
-
